cb746a57324a44f5b3c7f4ae3f6e154dd6a3f79a
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-12-17 22:34:42 +0100

    dist migration update

--- dist/migrations/1607799056775-migrationsFile.js
+++ dist/migrations/1607799056775-migrationsFile.js
@@ -1,10 +1,45 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.migrationsFile1607799056775 = void 0;
-class migrationsFile1607799056775 {
-    async up(queryRunner) {
+"use strict"
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value)
+          })
     }
-    async down(queryRunner) { }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value))
+        } catch (e) {
+          reject(e)
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected)
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next())
+    })
+  }
+Object.defineProperty(exports, "__esModule", { value: true })
+exports.migrationsFile1607716020421 = void 0
+class migrationsFile1607716020421 {
+  up(queryRunner) {
+    return __awaiter(this, void 0, void 0, function* () {})
+  }
+  down(queryRunner) {
+    return __awaiter(this, void 0, void 0, function* () {})
+  }
 }
-exports.migrationsFile1607799056775 = migrationsFile1607799056775;
-//# sourceMappingURL=1607799056775-migrationsFile.js.map
+exports.migrationsFile1607716020421 = migrationsFile1607716020421
