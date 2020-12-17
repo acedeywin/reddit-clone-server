ec81c4949596095469981448d0bbbaeedb720252
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-12-17 23:54:06 +0100

    migrations update

---
+++ dist/migrations/mainMigrations.js
@@ -0,0 +1,15 @@
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title UNIQUE NOT NULL, CONSTRAINT
+);
+
+CREATE TABLE user(
+        id SERIAL PRIMARY KEY NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        updated_at TIMESTAMP NOT NULL,
+        email UNIQUE NOT NULL,
+        username UNIQUE NOT NULL,
+        password NOT NULL,
+    );
