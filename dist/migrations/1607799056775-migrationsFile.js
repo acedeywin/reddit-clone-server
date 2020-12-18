9241c04dd27bca2b445212d2897cec104a7c0cc5
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-12-18 08:46:46 +0100

    .env update

---
+++ dist/migrations/1607799056775-migrationsFile.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.migrationsFile1607799056775 = void 0;
+class migrationsFile1607799056775 {
+    async up(queryRunner) {
+    }
+    async down(queryRunner) { }
+}
+exports.migrationsFile1607799056775 = migrationsFile1607799056775;
+//# sourceMappingURL=1607799056775-migrationsFile.js.map
