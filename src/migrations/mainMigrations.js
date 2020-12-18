1ac7e6e6b3421a87f48cc67ba026b1772a909876
Stephen Okpalaononuju <stephenokpala@gmail.com>
2020-12-18 17:04:03 +0100

    migrations update

--- src/migrations/mainMigrations.js
+++ src/migrations/mainMigrations.js
@@ -1,20 +1,20 @@
-CREATE TABLE post(
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    title VARCHAR (100) UNIQUE NOT NULL
-);
+// CREATE TABLE post(
+//     id SERIAL PRIMARY KEY,
+//     created_at TIMESTAMP NOT NULL,
+//     updated_at TIMESTAMP NOT NULL,
+//     title VARCHAR (100) UNIQUE NOT NULL
+// );
 
-CREATE TABLE "user"(
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    email VARCHAR (80) UNIQUE NOT NULL,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL
-);
+// CREATE TABLE "user"(
+//     id SERIAL PRIMARY KEY,
+//     created_at TIMESTAMP NOT NULL,
+//     updated_at TIMESTAMP NOT NULL,
+//     email VARCHAR (80) UNIQUE NOT NULL,
+//     username VARCHAR (50) UNIQUE NOT NULL,
+//     password VARCHAR (50) NOT NULL
+// );
 
-CREATE TABLE vote(
-    value INT NOT NULL,
-    postId INT NOT NULL
-);
+// CREATE TABLE vote(
+//     value INT NOT NULL,
+//     postId INT NOT NULL
+// );
